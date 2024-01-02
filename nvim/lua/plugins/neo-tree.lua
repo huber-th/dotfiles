9479810b2bcb72c9b06a7c869c316f6c94710f31
Thomas Huber <113915837+huber-th@users.noreply.github.com>
2024-01-02 13:34:07 -0800

    nvim: neotree show git ignored files dimmed

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,14 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          visible = false,
+          hide_gitignored = false,
+        }
+      }
+    }
     vim.keymap.set("n", "<leader>fe", ":Neotree filesystem reveal left<CR>", {})
     vim.keymap.set("n", "<leader>fq", ":Neotree toggle last<CR>", {})
     vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
