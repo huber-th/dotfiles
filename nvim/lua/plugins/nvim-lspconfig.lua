ed02abd3271a279a739d68520ecd33fa41682a3e
Thomas Huber <113915837+huber-th@users.noreply.github.com>
2024-01-23 20:56:29 -0800

    change(nvim) disable clang lsp

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -76,7 +76,7 @@ return {
     --  If you want to override the default filetypes that your language server will attach to you can
     --  define the property 'filetypes' to the map in question.
     local servers = {
-      clangd = {},
+      -- clangd = {},
       gopls = {},
       pyright = {},
       pylsp = {},
