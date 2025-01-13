ed89aaa18d0cd5e74cc8afb707ed4548038abd41
Thomas Huber <113915837+huber-th@users.noreply.github.com>
2025-01-12 21:40:31 -0800

    feat: Initial dotfiles

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,12 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+        options = {
+            theme = 'catppuccin-mocha',
+            icons_enabled = true,
+            component_separators = '|',
+            section_separators = '',
+        },
+    },
+}
